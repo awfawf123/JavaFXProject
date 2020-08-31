6f6b62cf10dbbe2bb30c6e67fcc79d97199f9647
ojh <awfawf123@naver.com>
2020-08-31 13:35:42 +0900

    8/31

--- JavaFXProject/src/basic/container/VboxExample.java
+++ JavaFXProject/src/basic/container/VboxExample.java
@@ -1,6 +1,8 @@
-package basic;
+package basic.container;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -22,7 +24,7 @@ public class VboxExample extends Application {
 		ImageView iv = new ImageView();
 		iv.setFitWidth(200);
 		iv.setPreserveRatio(true);
-		iv.setImage(new Image("/basic/images/q.jpg"));
+		iv.setImage(new Image("/basic/images/fruit.jpg"));
 		
 		HBox hbox =new HBox();
 		hbox.setAlignment(Pos.CENTER);
@@ -31,11 +33,26 @@ public class VboxExample extends Application {
 		Button btnPrev = new Button();
 		btnPrev.setText("이전");
 		Button btnNext = new Button("다음");
+		
 		HBox.setHgrow(btnNext, Priority.ALWAYS);
 		btnNext.setMaxWidth(Double.MAX_VALUE);
 		hbox.getChildren().add(btnPrev);
 		hbox.getChildren().add(btnNext);
 		VBox.setMargin(hbox, new Insets(10));
+		
+		//이벤트 핸들러를 해당 컨트롤에 등록
+		btnNext.setOnAction(new EventHandler<ActionEvent>() {
+	
+			int loc = 1;
+			@Override
+			public void handle(ActionEvent ae) {
+				if(loc == 9)
+					loc = 1;
+				iv.setImage(new Image("/basic/images/fruit"+ loc + ".jpg"));
+				
+			}
+			
+		});
 		
 		root.getChildren().add(iv);
 		root.getChildren().add(hbox);
