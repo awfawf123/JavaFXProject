a0432b845b5d2a42d40d3414061522457253b500
ojh <awfawf123@naver.com>
2020-09-01 13:41:50 +0900

    9/1

---
+++ JavaFXProject/src/basic/control/ViewController.java
@@ -0,0 +1,51 @@
+package basic.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+
+public class ViewController implements Initializable {
+	@FXML
+	private ListView<String> listView;
+	@FXML
+	private TableView<Phone> tableView;
+	@FXML
+	private ImageView imageView;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		listView.setItems(
+				FXCollections.observableArrayList("갤럭시S1", "갤럭시S2", "갤럭시S3", "갤럭시S4", "갤럭시S5", "갤럭시S6", "갤럭시S7"));
+		listView.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				tableView.getSelectionModel().select(newValue.intValue());
+				tableView.scrollTo(newValue.intValue());
+
+			}
+
+		});
+		
+		ObservableList phoneList = FXCollections.observableArrayList(
+				new Phone("갤럭시S1", "phone01.png"),
+				new Phone("갤럭시S2", "phone02.png"),
+				new Phone("갤럭시S3", "phone03.png"),
+				new Phone("갤럭시S4", "phone04.png"),
+				new Phone("갤럭시S5", "phone05.png"),
+				new Phone("갤럭시S6", "phone06.png"),
+				)
+
+	}
+	
+
+}
