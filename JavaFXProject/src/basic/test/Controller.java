5c52c8d29bca2ef8b3a285794ba20b3fbf24dcf0
ojh <awfawf123@naver.com>
2020-09-09 17:42:42 +0900

    9/9

--- JavaFXProject/src/basic/test/Controller.java
+++ JavaFXProject/src/basic/test/Controller.java
@@ -35,11 +35,10 @@ public class Controller implements Initializable {
 
 	ObservableList<Video> list;
 	ObservableList<Users> list1;
-	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		//Video
+		// Video
 		TableColumn<Video, ?> tc = tableView.getColumns().get(0);
 		tc.setCellValueFactory(new PropertyValueFactory<>("title"));
 
@@ -48,8 +47,8 @@ public class Controller implements Initializable {
 
 		tc = tableView.getColumns().get(2);
 		tc.setCellValueFactory(new PropertyValueFactory<>("price"));
-		
-		//Users
+
+		// Users
 		TableColumn<Users, ?> tc1 = tableView1.getColumns().get(0);
 		tc1.setCellValueFactory(new PropertyValueFactory<>("name"));
 
@@ -58,28 +57,30 @@ public class Controller implements Initializable {
 
 		tc1 = tableView1.getColumns().get(2);
 		tc1.setCellValueFactory(new PropertyValueFactory<>("number"));
-		
+
 		tc1 = tableView1.getColumns().get(3);
 		tc1.setCellValueFactory(new PropertyValueFactory<>("age"));
-		
+
 		list1 = FXCollections.observableArrayList();
 		list = FXCollections.observableArrayList();
-		
 
 		tableView.setItems(list);
 		tableView1.setItems(list1);
-		
 
 		btnAdd.setOnAction(event -> {
 			handleBtnAddAction();
 
 		});
-		
-		btnUserList.setOnAction(event ->{
+
+		btnUserList.setOnAction(event -> {
 			handleBtnUserListAction();
 		});
 
+		btnSearch.setOnAction(event -> {
+			handleBtnSearchAction();
+		});
 	}
+
 	public void handleBtnUserListAction() {
 		Stage stage = new Stage(StageStyle.UTILITY);
 		stage.initModality(Modality.WINDOW_MODAL);
@@ -91,8 +92,6 @@ public class Controller implements Initializable {
 			Scene scene = new Scene(parent);
 			stage.setScene(scene);
 			stage.show();
-			
-			
 
 			Button btnFormAdd = (Button) parent.lookup("#btnFormAdd");
 			btnFormAdd.setOnAction(new EventHandler<ActionEvent>() {
@@ -103,8 +102,7 @@ public class Controller implements Initializable {
 					TextField txtEmail = (TextField) parent.lookup("#txtEmail");
 					TextField txtNumber = (TextField) parent.lookup("#txtNumber");
 					TextField txtAge = (TextField) parent.lookup("#txtAge");
-					Users user = new Users(txtName.getText(),txtEmail.getText(),
-							txtNumber.getText(),
+					Users user = new Users(txtName.getText(), txtEmail.getText(), txtNumber.getText(),
 							Integer.parseInt(txtAge.getText()));
 
 					list1.add(user);
@@ -112,7 +110,7 @@ public class Controller implements Initializable {
 					stage.close();
 				}
 			});
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -139,7 +137,7 @@ public class Controller implements Initializable {
 					TextField txtTitle = (TextField) parent.lookup("#txtTitle");
 					TextField txtDirector = (TextField) parent.lookup("#txtDirector");
 					TextField txtPrice = (TextField) parent.lookup("#txtPrice");
-					Video video = new Video(txtTitle.getText(),txtDirector.getText(),
+					Video video = new Video(txtTitle.getText(), txtDirector.getText(),
 							Integer.parseInt(txtPrice.getText()));
 
 					list.add(video);
@@ -153,5 +151,41 @@ public class Controller implements Initializable {
 
 	}
 
-	
+	public void handleBtnSearchAction() {
+		Stage stage = new Stage(StageStyle.UTILITY);
+		stage.initModality(Modality.WINDOW_MODAL);
+		stage.initOwner(btnSearch.getScene().getWindow());
+
+		try {
+			Parent parent = FXMLLoader.load(getClass().getResource("Search.fxml"));
+
+			Scene scene = new Scene(parent);
+			stage.setScene(scene);
+			stage.show();
+
+			Button btns = (Button) parent.lookup("#btns");
+			btns.setOnAction(new EventHandler<ActionEvent>() {
+
+				@Override
+				public void handle(ActionEvent arg0) {
+					TextField txtTitle = (TextField) parent.lookup("#txtTitle");
+					TextField txtDirector = (TextField) parent.lookup("#txtDirector");
+					TextField txtPrice = (TextField) parent.lookup("#txtPrice");
+
+					for (Video vi : list) {
+						if (vi.getTitle().equals(txtTitle.getText())) {
+							txtDirector.setText(vi.getDirector());
+							txtPrice.setText(String.valueOf(vi.getPrice()));
+						}
+					}
+
+				}
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
